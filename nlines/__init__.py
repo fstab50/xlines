ba44891d487b0ecd03a1a09257c2c9b3b4bf36cb
Blake Huber <blakeca00@gmail.com>
2019-03-12 16:33:55 -0500

    nlines/__init__.py

--- nlines/__init__.py
+++ nlines/__init__.py
@@ -2,34 +2,12 @@ from pyaws import logd
 from nlines._version import __version__ as version
 from nlines.statics import local_config
 
+
 __author__ = 'Blake Huber'
 __version__ = version
 __email__ = "blakeca00@gmail.com"
 
-"""
-PACKAGE = 'nlines'
-enable_logging = True
-log_mode = 'FILE'          # log to cloudwatch logs
-log_filename = 'nlines.log'
-log_dir = '/tmp'
-log_path = log_dir + '/' + log_filename
 
-
-log_config = {
-    "PROJECT": {
-        "PACKAGE": PACKAGE,
-        "CONFIG_VERSION": __version__,
-    },
-    "LOGGING": {
-        "ENABLE_LOGGING": enable_logging,
-        "LOG_FILENAME": log_filename,
-        "LOG_DIR": log_dir,
-        "LOG_PATH": log_path,
-        "LOG_MODE": log_mode,
-        "SYSLOG_FILE": False
-    }
-}
-"""
 # global logger
 logd.local_config = local_config
 logger = logd.getLogger(__version__)
