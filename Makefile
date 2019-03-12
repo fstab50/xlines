bd01449d92ff118164c5657c6bb7cff1d905b519
Blake Huber <blakeca00@gmail.com>
2019-03-12 07:35:15 -0500

    adds

---
+++ Makefile
@@ -0,0 +1,141 @@
+#
+#	 Makefile, ver 1.6, PROJECT:  keyup
+#
+# --- declarations  --------------------------------------------------------------------------------
+
+
+PROJECT := nlines
+CUR_DIR = $(shell pwd)
+PYTHON_VERSION := python3
+PYTHON3_PATH := $(shell which $(PYTHON_VERSION))
+GIT := $(shell which git)
+VENV_DIR := $(CUR_DIR)/p3_venv
+PIP_CALL := $(VENV_DIR)/bin/pip
+ACTIVATE = $(shell . $(VENV_DIR)/bin/activate)
+MAKE = $(shell which make)
+MODULE_PATH := $(CUR_DIR)/$(PROJECT)
+SCRIPTS := $(CUR_DIR)/scripts
+DOC_PATH := $(CUR_DIR)/docs
+REQUIREMENT = $(CUR_DIR)/requirements.txt
+VERSION_FILE = $(CUR_DIR)/$(PROJECT)/_version.py
+
+
+# --- rollup targets  ------------------------------------------------------------------------------
+
+
+.PHONY: fresh-install fresh-test-install deploy-test deploy-prod
+
+zero-source-install: clean source-install   ## Install (source: local). Zero prebuild artifacts
+
+zero-test-install: clean setup-venv test-install  ## Install (source: testpypi). Zero prebuild artifacts
+
+deploy-test: clean testpypi  ## Deploy (testpypi), generate all prebuild artifacts
+
+deploy-prod: clean pypi   ## Deploy (pypi), generate all prebuild artifacts
+
+
+# --- targets -------------------------------------------------------------------------------------
+
+
+.PHONY: pre-build
+pre-build:    ## Remove residual build artifacts
+	rm -rf $(CUR_DIR)/dist
+	mkdir $(CUR_DIR)/dist
+
+
+.PHONY: setup-venv
+setup-venv:    ## Create and activiate python venv
+	$(PYTHON3_PATH) -m venv $(VENV_DIR)
+	. $(VENV_DIR)/bin/activate && $(PIP_CALL) install -U setuptools pip && \
+	$(PIP_CALL) install -r $(REQUIREMENT)
+
+
+.PHONY: test
+test:     ## Run pytest unittests
+	if [ $(PDB) ]; then PDB = "true"; \
+	bash $(CUR_DIR)/scripts/make-test.sh $(CUR_DIR) $(VENV_DIR) $(MODULE_PATH) $(PDB); \
+	elif [ $(MODULE) ]; then PDB = "false"; \
+	bash $(CUR_DIR)/scripts/make-test.sh $(CUR_DIR) $(VENV_DIR) $(MODULE_PATH) $(PDB) $(MODULE); \
+	elif [ $(COMPLEXITY) ]; then COMPLEXITY = "run"; \
+	bash $(CUR_DIR)/scripts/make-test.sh $(CUR_DIR) $(VENV_DIR) $(MODULE_PATH) $(COMPLEXITY) $(MODULE); \
+	else bash $(CUR_DIR)/scripts/make-test.sh $(CUR_DIR) $(VENV_DIR) $(MODULE_PATH); fi
+
+
+docs:  setup-venv    ## Generate sphinx documentation
+	. $(VENV_DIR)/bin/activate && \
+	$(PIP_CALL) install sphinx sphinx_rtd_theme autodoc
+	cd $(CUR_DIR) && $(MAKE) clean-docs
+	cd $(DOC_PATH) && . $(VENV_DIR)/bin/activate && $(MAKE) html
+
+
+.PHONY: build
+build: pre-build setup-venv    ## Build dist, increment version || force version (VERSION=X.Y)
+	if [ $(VERSION) ]; then bash $(SCRIPTS)/version_update.sh $(VERSION); \
+	else bash $(SCRIPTS)/version_update.sh; fi && . $(VENV_DIR)/bin/activate && \
+	cd $(CUR_DIR) && $(PYTHON3_PATH) setup.py sdist
+
+
+.PHONY: testpypi
+testpypi: build     ## Deploy to testpypi without regenerating prebuild artifacts
+	@echo "Deploy $(PROJECT) to test.pypi.org"
+	. $(VENV_DIR)/bin/activate && twine upload --repository testpypi dist/*
+
+
+.PHONY: pypi
+pypi: clean build    ## Deploy to pypi without regenerating prebuild artifacts
+	@echo "Deploy $(PROJECT) to pypi.org"
+	. $(VENV_DIR)/bin/activate && twine upload --repository pypi dist/*
+
+
+.PHONY: install
+install:    ## Install (source: pypi). Build artifacts exist
+	if [ ! -e $(VENV_DIR) ]; then $(MAKE) setup-venv; fi; \
+	cd $(CUR_DIR) && . $(VENV_DIR)/bin/activate && \
+	$(PIP_CALL) install -U $(PROJECT)
+
+
+.PHONY: test-install
+test-install:  ## Install (source: testpypi). Build artifacts exist
+	if [ ! -e $(VENV_DIR) ]; then $(MAKE) setup-venv; fi; \
+	cd $(CUR_DIR) && . $(VENV_DIR)/bin/activate && \
+	$(PIP_CALL) install -U $(PROJECT) --extra-index-url https://test.pypi.org/simple/
+
+
+.PHONY: source-install
+source-install:    ## Install (source: local source). Build artifacts exist
+	if [ ! -e $(VENV_DIR) ]; then $(MAKE) setup-venv; fi; \
+	cd $(CUR_DIR) && . $(VENV_DIR)/bin/activate && \
+	$(PIP_CALL) install .
+
+
+.PHONY: update-source-install
+update-source-install:    ## Update Install (source: local source).
+	if [ -e $(VENV_DIR) ]; then \
+	cp -rv $(MODULE_PATH) $(VENV_DIR)/lib/python3.6/site-packages/; fi
+
+
+.PHONY: help
+help:   ## Print help index
+	@printf "\n\033[0m %-15s\033[0m %-13s\u001b[37;1m%-15s\u001b[0m\n\n" " " "make targets: " $(PROJECT)
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {sub("\\\\n",sprintf("\n%22c"," "), $$2);printf "\033[0m%-2s\033[36m%-20s\033[33m %-8s\033[0m%-5s\n\n"," ", $$1, "-->", $$2}' $(MAKEFILE_LIST)
+	@printf "\u001b[37;0m%-2s\u001b[37;0m%-2s\n\n" " " "___________________________________________________________________"
+	@printf "\u001b[37;1m%-3s\u001b[37;1m%-3s\033[0m %-6s\u001b[44;1m%-9s\u001b[37;0m%-15s\n\n" " " "  make" "deploy-[test|prod] " "VERSION=X" " to deploy specific version"
+
+
+.PHONY: clean-docs
+clean-docs:    ## Remove build artifacts for documentation only
+	@echo "Clean docs build directory"
+	if [ -e $(VENV_DIR) ]; then . $(VENV_DIR)/bin/activate && \
+	cd $(DOC_PATH) && $(MAKE) clean || true; fi
+
+
+.PHONY: clean
+clean:  clean-docs  ## Remove all build artifacts generated by make
+	@echo "Clean project directories"
+	rm -rf $(VENV_DIR) || true
+	rm -rf $(CUR_DIR)/dist || true
+	rm -rf $(CUR_DIR)/*.egg-info || true
+	rm -f $(CUR_DIR)/README.rst || true
+	rm -rf $(CUR_DIR)/$(PROJECT)/__pycache__ || true
+	rm -rf $(CUR_DIR)/tests/__pycache__ || true
+	rm -rf $(CUR_DIR)/.pytest_cache || true
---
+++ setup.py
@@ -0,0 +1,210 @@
+"""
+
+nlines :  Copyright 2017-2018, Blake Huber
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+see: https://www.gnu.org/licenses/#GPL
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+contained in the program LICENSE file.
+
+"""
+
+import os
+import sys
+import platform
+import subprocess
+from shutil import which
+from shutil import copy2 as copyfile
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from codecs import open
+import nlines
+
+
+requires = [
+    'pyaws>=0.2.28',
+    'veryprettytable>=0.8.1'
+]
+
+
+_root = os.path.abspath(os.path.dirname(__file__))
+_comp_fname = 'nlines-completion.bash'
+
+
+def _root_user():
+    """
+    Checks localhost root or sudo access
+    """
+    if os.geteuid() == 0:
+        return True
+    elif subprocess.getoutput('echo $EUID') == '0':
+        return True
+    return False
+
+
+def create_artifact(object_path, type):
+    """Creates post install filesystem artifacts"""
+    if type == 'file':
+        with open(object_path, 'w') as f1:
+            f1.write(sourcefile_content())
+    elif type == 'dir':
+        os.makedirs(object_path)
+
+
+def os_parityPath(path):
+    """
+    Converts unix paths to correct windows equivalents.
+    Unix native paths remain unchanged (no effect)
+    """
+    path = os.path.normpath(os.path.expanduser(path))
+    if path.startswith('\\'):
+        return 'C:' + path
+    return path
+
+
+class PostInstallDevelop(develop):
+    """ post-install, development """
+    def run(self):
+        subprocess.check_call("bash scripts/post-install-dev.sh".split())
+        develop.run(self)
+
+
+class PostInstall(install):
+    """
+    Summary.
+
+        Postinstall script to place bash completion artifacts
+        on local filesystem
+
+    """
+    def valid_os_shell(self):
+        """
+        Summary.
+
+            Validates install environment for Linux and Bash shell
+
+        Returns:
+            Success | Failure, TYPE bool
+
+        """
+        if platform.system() == 'Windows':
+            return False
+        elif which('bash'):
+            return True
+        elif 'bash' in subprocess.getoutput('echo $SHELL'):
+            return True
+        return False
+
+    def run(self):
+        """
+        Summary.
+
+            Executes post installation configuration only if correct
+            environment detected
+
+        """
+        if self.valid_os_shell():
+
+            completion_file = user_home() + '/.bash_completion'
+            completion_dir = user_home() + '/.bash_completion.d'
+            config_dir = user_home() + '/.config/nlines'
+
+            if not os.path.exists(os_parityPath(completion_file)):
+                create_artifact(os_parityPath(completion_file), 'file')
+            if not os.path.exists(os_parityPath(completion_dir)):
+                create_artifact(os_parityPath(completion_dir), 'dir')
+            if not os.path.exists(os_parityPath(config_dir)):
+                create_artifact(os_parityPath(config_dir), 'dir')
+
+            if _root_user():
+                copyfile(
+                        completion_dir + '/' + _comp_fname,
+                        '/etc/bash_completion.d/' + _comp_fname,
+                    )
+                os.remove(completion_dir + '/' + _comp_fname)
+        install.run(self)
+
+
+def preclean(dst):
+    if os.path.exists(dst):
+        os.remove(dst)
+    return True
+
+
+def read(fname):
+    basedir = os.path.dirname(sys.argv[0])
+    return open(os.path.join(basedir, fname)).read()
+
+
+def sourcefile_content():
+    sourcefile = """
+    for bcfile in ~/.bash_completion.d/* ; do
+        [ -f "$bcfile" ] && . $bcfile
+    done\n
+    """
+    return sourcefile
+
+
+def user_home():
+    """Returns os specific home dir for current user"""
+    try:
+        if platform.system() == 'Linux':
+            return os.getenv('HOME')
+
+        elif platform.system() == 'Windows':
+            username = os.getenv('username')
+            return 'C:\\Users\\' + username
+
+        elif platform.system() == 'Java':
+            print(f'Unsupported of {os_type}')
+            sys.exit(1)
+    except OSError as e:
+        raise e
+
+
+setup(
+    name='nlines',
+    version=nlines.__version__,
+    description='Count the number of lines of code in a project',
+    long_description=read('DESCRIPTION.rst'),
+    url='',
+    author=nlines.__author__,
+    author_email=nlines.__email__,
+    license='GPL-3.0',
+    classifiers=[
+        'Topic :: Security',
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+    ],
+    keywords='Amazon Web Services iam credentials AWS access key secret key',
+    packages=find_packages(exclude=['assets', 'docs', 'reports', 'scripts', 'tests']),
+    include_package_data=True,
+    install_requires=requires,
+    python_requires='>=3.6, <4',
+    cmdclass={
+        'install': PostInstall
+    },
+    data_files=[
+        (user_home() + '/' + '.bash_completion.d', ['bash/' + _comp_fname])
+    ],
+    entry_points={
+        'console_scripts': [
+            'nlines=nlines.cli:init_cli'
+        ]
+    },
+    zip_safe=False
+)
