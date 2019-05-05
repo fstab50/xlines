b0c85213d017ba5659f5ace4203eabd5842ebef7
Blake Huber <blakeca00@gmail.com>
2019-05-05 06:20:48 -0500

    scripts/upload-s3-artifacts.sh: updated for local iamuser admin

--- scripts/upload-s3-artifacts.sh
+++ scripts/upload-s3-artifacts.sh
@@ -4,6 +4,8 @@ PROFILE='imagestore'
 BUCKET='http-imagestore'
 KEY='xlines'
 
+pkg_path=$(cd $(dirname $0); pwd -P)
+source $pkg_path/colors.sh
 
 
 function _git_root(){
@@ -14,26 +16,41 @@ function _git_root(){
 }
 
 
+function _valid_iamuser(){
+    ##
+    ##  use Amazon STS to validate credentials of iam user
+    ##
+    local iamuser="$1"
+
+    if [[ $(aws sts get-caller-identity --profile $PROFILE 2>/dev/null) ]]; then
+        return 0
+    fi
+    return 1
+}
 
 
 ROOT=$(_git_root)
 
-if [[ "$(gcreds -s | grep $PROFILE)" ]] && [[ ! "$(gcreds -s | grep expired)" ]]; then
 
+if _valid_iamuser $PROFILE; then
+
+    printf -- '\n'
     cd "$ROOT/assets"
 
     for i in $(ls .); do
 
         # upload object
+        printf -- '\n%s\n' "s3 object $BOLD$i$UNBOLD uploaded..."
         aws --profile $PROFILE s3 cp ./$i s3://$BUCKET/$KEY/$i
-        echo "s3 object $i uploaded..."
 
         aws --profile $PROFILE s3api put-object-acl --acl 'public-read' --bucket $BUCKET --key $KEY/$i
-        echo "s3 object acl applied to $i..."
+        printf -- '%s\n' "s3 object acl applied to $i..."
 
     done
+
+    printf -- '\n'
     cd "$ROOT" || true
 
 else
-    echo "You must rerun gcreds to generate temporary credentials for $(PROFILE)"
+    echo "You must ensure $PROFILE is present in the local awscli configuration"
 fi
