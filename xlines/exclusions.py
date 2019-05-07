2653ff2b3fa91868f4b091fa0cabfd70be4020f8
Blake Huber <blakeca00@gmail.com>
2019-05-07 06:29:26 -0500

    xlines/exclusions.py add

---
+++ xlines/exclusions.py
@@ -0,0 +1,76 @@
+"""
+Summary.
+
+    Exclusions Module -- Display, Update, and Modification
+    of objects to be excluded from line count totals
+
+"""
+import os
+import inspect
+from veryprettytable import VeryPrettyTable
+from pyaws.colors import Colors
+from xlines.colormap import ColorMap
+from xlines import logger
+from xlines.statics import local_config
+
+cm = ColorMap()
+
+try:
+    from pyaws.core.oscodes_unix import exit_codes
+    os_type = 'Linux'
+    user_home = os.getenv('HOME')
+    splitchar = '/'                             # character for splitting paths (linux)
+    acct = cm.accent
+    text = Colors.BRIGHT_PURPLE
+except Exception:
+    from pyaws.core.oscodes_win import exit_codes    # non-specific os-safe codes
+    os_type = 'Windows'
+    user_home = os.getenv('username')
+    splitchar = '\\'                            # character for splitting paths (windows)
+    acct = cm.bbc
+    text = Colors.LT2GRAY
+
+
+
+# universal colors
+rd = Colors.RED + Colors.BOLD
+yl = Colors.YELLOW + Colors.BOLD
+fs = Colors.GOLD3
+bd = cm.bd
+gn = Colors.BRIGHT_GREEN
+title = Colors.BRIGHT_WHITE + Colors.BOLD
+bbc = bd + Colors.BRIGHT_CYAN
+frame = gn + bd
+btext = text + Colors.BOLD
+bwt = Colors.BRIGHT_WHITE
+bdwt = Colors.BOLD + Colors.BRIGHT_WHITE
+ub = Colors.UNBOLD
+rst = Colors.RESET
+
+# globals
+container = []
+config_dir = local_config['CONFIG']['CONFIG_PATH']
+expath = local_config['EXCLUSIONS']['EX_EXT_PATH']
+BUFFER = local_config['PROJECT']['BUFFER']
+
+
+class ExcludedTypes():
+    def __init__(self, ex_path, ex_container=[]):
+        self.types = ex_container
+        if not self.types:
+            self.types.extend(self.parse_exclusions(ex_path))
+
+    def excluded(self, path):
+        for i in self.types:
+            if i in path:
+                return True
+        return False
+
+    def parse_exclusions(self, path):
+        """
+        Parse persistent fs location store for file extensions to exclude
+        """
+        try:
+            return [x.strip() for x in open(path).readlines()]
+        except OSError:
+            return []
