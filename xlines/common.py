a1656b87a648c84d2d56068c885278bbe4141eba
Blake Huber <blakeca00@gmail.com>
2019-06-01 14:43:09 -0500

    xlines/common.py: updated with module function from pyaws lib dependency

--- xlines/common.py
+++ xlines/common.py
@@ -5,9 +5,11 @@ Summary.
 
 """
 import os
+import json
 import inspect
 import platform
-from pyaws.colors import Colors
+from pathlib import Path
+from xlines import Colors
 from xlines import logger
 from xlines.statics import local_config
 
@@ -213,6 +215,124 @@ def locate_fileobjects(origin, path=expath):
                 )
                 continue
     return remove_duplicates(fobjects)
+
+
+def get_os(detailed=False):
+    """
+    Summary:
+        Retrieve local operating system environment characteristics
+    Args:
+        :user (str): USERNAME, only required when run on windows os
+    Returns:
+        TYPE: dict object containing key, value pairs describing
+        os information
+    """
+    try:
+
+        os_type = platform.system()
+
+        if os_type == 'Linux':
+            os_detail = platform.platform()
+            distribution = platform.linux_distribution()[0]
+            HOME = str(Path.home())
+            username = os.getenv('USER')
+        elif os_type == 'Windows':
+            os_detail = platform.platform()
+            username = os.getenv('username') or os.getenv('USER')
+            HOME = 'C:\\Users\\' + username
+        else:
+            logger.warning('Unsupported OS. No information')
+            os_type = 'Java'
+            os_detail = 'unknown'
+            HOME = os.getenv('HOME')
+            username = os.getenv('USER')
+
+    except OSError as e:
+        raise e
+    except Exception as e:
+        logger.exception(
+            '%s: problem determining local os environment %s' %
+            (inspect.stack()[0][3], str(e))
+            )
+    if detailed and os_type == 'Linux':
+        return {
+                'os_type': os_type,
+                'os_detail': os_detail,
+                'linux_distribution': distribution,
+                'username': username,
+                'HOME': HOME
+            }
+    elif detailed:
+        return {
+                'os_type': os_type,
+                'os_detail': os_detail,
+                'username': username,
+                'HOME': HOME
+            }
+    return {'os_type': os_type}
+
+
+def import_file_object(filename):
+    """
+    Summary:
+        Imports block filesystem object
+    Args:
+        :filename (str): block filesystem object
+    Returns:
+        dictionary obj (valid json file), file data object
+    """
+    try:
+        handle = open(filename, 'r')
+        file_obj = handle.read()
+        dict_obj = json.loads(file_obj)
+
+    except OSError as e:
+        logger.critical(
+            'import_file_object: %s error opening %s' % (str(e), str(filename))
+        )
+        raise e
+    except ValueError:
+        logger.info(
+            '%s: import_file_object: %s not json. file object returned' %
+            (inspect.stack()[0][3], str(filename))
+        )
+        return file_obj    # reg file, not valid json
+    return dict_obj
+
+
+def read_local_config(cfg):
+    """ Parses local config file for override values
+
+    Args:
+        :local_file (str):  filename of local config file
+
+    Returns:
+        dict object of values contained in local config file
+    """
+    try:
+        if os.path.exists(cfg):
+            config = import_file_object(cfg)
+            return config
+        else:
+            logger.warning(
+                '%s: local config file (%s) not found, cannot be read' %
+                (inspect.stack()[0][3], str(cfg)))
+    except OSError as e:
+        logger.warning(
+            'import_file_object: %s error opening %s' % (str(e), str(cfg))
+        )
+    return {}
+
+
+def os_parityPath(path):
+    """
+    Converts unix paths to correct windows equivalents.
+    Unix native paths remain unchanged (no effect)
+    """
+    path = os.path.normpath(os.path.expanduser(path))
+    if path.startswith('\\'):
+        return 'C:' + path
+    return path
 
 
 def user_home():
