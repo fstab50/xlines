e889afd1cbe4ac6a34f6821f06a3f331e3dafe6f
Blake Huber <blakeca00@gmail.com>
2019-06-03 08:32:02 -0500

    xlines/cli.py: debug fixes for global scoped variables set in module function precheck

--- xlines/cli.py
+++ xlines/cli.py
@@ -79,8 +79,6 @@ rst = cm.rst
 # globals
 container = []
 config_dir = local_config['CONFIG']['CONFIG_PATH']
-#expath = local_config['EXCLUSIONS']['EX_EXT_PATH']
-#exdirpath = local_config['EXCLUSIONS']['EX_DIR_PATH']
 div = text + '/' + rst
 div_len = 2
 horiz = text + '-' + rst
@@ -413,8 +411,8 @@ def create_container(parameters):
 
 
 def init_cli():
-    expath = local_config['EXCLUSIONS']['EX_EXT_PATH']
-    exdirpath = local_config['EXCLUSIONS']['EX_DIR_PATH']
+    ex_files = local_config['EXCLUSIONS']['EX_EXT_PATH']
+    ex_dirs = local_config['EXCLUSIONS']['EX_DIR_PATH']
     parser = argparse.ArgumentParser(add_help=False)
 
     try:
@@ -441,7 +439,7 @@ def init_cli():
         help_menu()
         sys.exit(exit_codes['EX_OK']['Code'])
 
-    elif args.sum and precheck(expath, exdirpath):
+    elif args.sum and precheck(ex_files, ex_dirs):
 
         ex = ExcludedTypes(ex_path=str(Path.home()) + '/.config/xlines/exclusions.list')
         container = create_container(args.sum)
