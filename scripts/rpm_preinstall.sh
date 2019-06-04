a1e97d5e9ccf561e01eb84a9a2b799103d0fc42b
Blake Huber <blakeca00@gmail.com>
2019-06-04 08:08:43 -0500

    scripts/rpm_preinstall.sh: updated for rhel, centos

--- scripts/rpm_preinstall.sh
+++ scripts/rpm_preinstall.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 """
 
 xlines :  Copyright 2018-2019, Blake Huber
@@ -31,94 +32,48 @@ from pathlib import Path
 from codecs import open
 
 
-_project = 'xlines'
-_root = os.path.abspath(os.path.dirname(__file__))
-_ex_fname = 'exclusions.list'
-_ex_dirs_fname = 'directories.list'
-_comp_fname = 'xlines-completion.bash'
-_homedir = str(Path.home())
+_project='xlines'
 
 
-def create_artifact(object_path, type):
-    """Creates post install filesystem artifacts"""
-    if type == 'file':
-        with open(object_path, 'w') as f1:
-            f1.write(sourcefile_content())
-    elif type == 'dir':
-        os.makedirs(object_path)
+function _redhat_linux(){
+    ##
+    ##  determines if Redhat Enterprise Linux, Centos
+    ##
+    if [[ -f /etc/redhat-release ]]; then
+        return 0
+
+    elif [[ $(grep -i centos /etc/os-release) ]]; then
+        return 0
+
+    elif [[ $(grep -i redhat /etc/os-release) ]]; then
+        return 0
+    fi
+
+    return 1
+}
 
 
-def os_parityPath(path):
-    """
-    Converts unix paths to correct windows equivalents.
-    Unix native paths remain unchanged (no effect)
-    """
-    path = os.path.normpath(os.path.expanduser(path))
-    if path.startswith('\\'):
-        return 'C:' + path
-    return path
+function _amazon_linux(){
+    ##
+    ##  determines if Amazon Linux
+    ##
+    if [[ -f /etc/redhat-release ]]; then
+        return 0
+
+    elif [[ $(grep -i centos /etc/os-release) ]]; then
+        return 0
+
+    elif [[ $(grep -i redhat /etc/os-release) ]]; then
+        return 0
+    fi
+
+    return 1
+}
 
 
-class PostInstall(install):
-    """
-    Summary.
+if _redhat_linux; then
 
-        Postinstall script to place bash completion artifacts
-        on local filesystem
+    yum -y install epel-release || exit 1
+    exit 0
 
-    """
-    def valid_os_shell(self):
-        """
-        Summary.
-
-            Validates install environment for Linux and Bash shell
-
-        Returns:
-            Success | Failure, TYPE bool
-
-        """
-        if platform.system() == 'Windows':
-            return False
-        elif which('bash'):
-            return True
-        elif 'bash' in subprocess.getoutput('echo $SHELL'):
-            return True
-        return False
-
-    def run(self):
-        """
-        Summary.
-
-            Executes post installation configuration only if correct
-            environment detected
-
-        """
-        if self.valid_os_shell():
-
-            completion_dir = '/etc/bash_completion.d'
-            config_dir = user_home() + '/.config/' + _project
-
-            if not os.path.exists(completion_dir):
-                create_artifact(completion_dir, 'dir')
-            if not os.path.exists(config_dir):
-                create_artifact(config_dir, 'dir')
-
-            # ensure installation of home directory profile artifacts (data_files)
-            if not os.path.exists(os_parityPath(completion_dir + '/' + _comp_fname)):
-                copyfile(
-                    os_parityPath('bash' + '/' + _comp_fname),
-                    os_parityPath(completion_dir + '/' + _comp_fname)
-                )
-            if not os.path.exists(os_parityPath(config_dir + '/' + _ex_fname)):
-                copyfile(
-                    os_parityPath('config' + '/' + _ex_fname),
-                    os_parityPath(config_dir + '/' + _ex_fname)
-                )
-            if not os.path.exists(os_parityPath(config_dir + '/' + _ex_dirs_fname)):
-                copyfile(
-                    os_parityPath('config' + '/' + _ex_dirs_fname),
-                    os_parityPath(config_dir + '/' + _ex_dirs_fname)
-                )
-        install.run(self)
-
-PostInstall()
+fi
