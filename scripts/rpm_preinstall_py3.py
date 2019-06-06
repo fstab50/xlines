398ed1ae3a0c3e89af0aabd38d5b17da1d556433
Blake Huber <blakeca00@gmail.com>
2019-06-06 16:55:34 -0500

    scripts/rpm_preinstall_py3.py: docstring

--- scripts/rpm_preinstall_py3.py
+++ scripts/rpm_preinstall_py3.py
@@ -1,24 +1,26 @@
 #!/bin/python3
 
 
-#  
-#  xlines :  Copyright 2018-2019, Blake Huber
-#  
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#  
-#  see: https://www.gnu.org/licenses/#GPL
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  contained in the program LICENSE file.
-#  
+"""
+
+  xlines :  Copyright 2018-2019, Blake Huber
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  see: https://www.gnu.org/licenses/#GPL
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  contained in the program LICENSE file.
+
+"""
 
 import os
 import sys
