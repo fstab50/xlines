c8eca5c83a35e623ce29376a682cc557aac5713b
Blake Huber <blakeca00@gmail.com>
2019-06-16 06:46:13 -0500

    xlines/cli.py: convert to use variables.py module containing global assignments

--- xlines/core.py
+++ xlines/core.py
@@ -32,31 +32,6 @@ except Exception:
     text = Colors.LT2GRAY
     TITLE = Colors.WHITE + Colors.BOLD
 
-"""
-# universal colors
-rd = Colors.RED + Colors.BOLD
-yl = Colors.YELLOW + Colors.BOLD
-fs = Colors.GOLD3
-bd = Colors.BOLD
-gn = Colors.BRIGHT_GREEN
-title = Colors.BRIGHT_WHITE + Colors.BOLD
-highlight = bd + Colors.BRIGHT_YELLOW2
-bbc = bd + Colors.BRIGHT_CYAN
-frame = gn + bd
-btext = text + Colors.BOLD
-bwt = Colors.BRIGHT_WHITE
-bdwt = Colors.BOLD + Colors.BRIGHT_WHITE
-ub = Colors.UNBOLD
-rst = Colors.RESET
-
-# globals
-expath = local_config['EXCLUSIONS']['EX_EXT_PATH']
-div = text + '/' + rst
-div_len = 2
-horiz = text + '-' + rst
-arrow = bwt + '-> ' + rst
-BUFFER = local_config['OUTPUT']['BUFFER']
-"""
 
 def linecount(path, whitespace=True):
     if whitespace:
