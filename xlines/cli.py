baad792c0acc909eae3137572cc2ccf3854b2909
Blake Huber <blakeca00@gmail.com>
2019-06-16 06:36:09 -0500

    xlines/cli.py: convert to use variables.py module containing global assignments

--- xlines/cli.py
+++ xlines/cli.py
@@ -40,6 +40,7 @@ from xlines.help_menu import menu_body
 from xlines.mp import multiprocessing_main
 from xlines.core import linecount, locate_fileobjects, remove_illegal, print_footer, print_header
 from xlines.colormap import ColorMap
+from xlines.variables import *
 
 
 cm = ColorMap()
@@ -61,30 +62,8 @@ except Exception:
     text = Colors.LT2GRAY
     TITLE = Colors.WHITE + Colors.BOLD
 
-# universal colors
-rd = Colors.RED + Colors.BOLD
-yl = Colors.YELLOW + Colors.BOLD
-fs = Colors.GOLD3
-bd = Colors.BOLD
-gn = Colors.BRIGHT_GREEN
-title = Colors.BRIGHT_WHITE + Colors.BOLD
-bbc = bd + Colors.BRIGHT_CYAN
-highlight = bd + Colors.BRIGHT_YELLOW2
-frame = gn + bd
-btext = text + Colors.BOLD
-bwt = Colors.BRIGHT_WHITE
-bdwt = Colors.BOLD + Colors.BRIGHT_WHITE
-ub = Colors.UNBOLD
-rst = cm.rst
-
 # globals
 container = []
-config_dir = local_config['CONFIG']['CONFIG_PATH']
-div = text + '/' + rst
-div_len = 2
-horiz = text + '-' + rst
-arrow = bwt + '-> ' + rst
-BUFFER = local_config['OUTPUT']['BUFFER']
 
 
 def _configure(expath, exdirpath):
