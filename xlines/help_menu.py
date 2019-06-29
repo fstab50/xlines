262bb946922d6c2741117c5575472c774d39d329
Blake Huber <blakeca00@gmail.com>
2019-06-29 08:31:25 -0500

    help menu updates

--- xlines/help_menu.py
+++ xlines/help_menu.py
@@ -76,9 +76,9 @@ menu_body = menu_title + c.BOLD + """
   LEGEND""" + rst + """
 
            """ + bbl + 'o' + rst + """  |  Filesystem object counted (""" + bcy + 'cyan' + rst + """)
-        --------------------------------------------------------
+        ---------------------------------------------------------
            """ + bdacct + 'o' + rst + """  |  Line count above high ct threshold (""" + acct + 'orange' + rst + """)
-        --------------------------------------------------------
+        ---------------------------------------------------------
           """ + bwt + '->' + rst + """  |  Truncated (shortened) file path (""" + bwt + 'white' + rst + """)
 
     """
