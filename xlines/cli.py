9c8c7124e1470838db5c95b37db2bdc93b3a3f28
Blake Huber <blakeca00@gmail.com>
2019-07-25 13:43:57 -0500

    xlines/cli.py: attempts at runtime environment lang setting

--- xlines/cli.py
+++ xlines/cli.py
@@ -265,7 +265,7 @@ def precheck(user_exfiles, user_exdirs, debug):
     Runtime Dependency Checks: postinstall artifacts, environment
     """
     def set_environment():
-        exitcode = 1
+        exitcode = 1; lanaguage = None
         if os.getenv('LANG') is None:
             lanaguge =  'export LANG=en_US.UTF-8'
         elif 'UTF-8' not in subprocess.getoutput('echo $LANG'):
