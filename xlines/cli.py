75c90ec87b5ccbe92b66eaa565bd0f818964a386
Blake Huber <blakeca00@gmail.com>
2019-07-25 13:47:15 -0500

    xlines/cli.py: attempts at runtime environment lang setting

--- xlines/cli.py
+++ xlines/cli.py
@@ -285,7 +285,7 @@ def precheck(user_exfiles, user_exdirs, debug):
         stdout_message(f'_os_dir_fname: {_os_dir_fname}: system directories.list file path', 'DBUG')
         stdout_message(f'_configdir: {_config_dir}: user home config file location', 'DBUG')
         stdout_message(f'Environment setup status: {_environment_setup}')
-        stdout_message('{}Add the following code to your .bashrc file:\n{}{}'.format(tab, tab, language))
+        stdout_message('{}Add the following code to your .bashrc file:\n{}{}'.format(tab, tab, str(language)))
     try:
         # check if exists; copy
         if not os.path.exists(_config_dir):
