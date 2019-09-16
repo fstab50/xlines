4f3a9e1c59ba5f4dca5f71150b67627c8748b03f
Blake Huber <blakeca00@gmail.com>
2019-09-16 13:30:55 -0500

    debug run containiner --user option

--- packaging/docker/test-centos.sh
+++ packaging/docker/test-centos.sh
@@ -58,11 +58,7 @@ docker build  -t  $image .
 
 # create container
 std_message "Creating and running container ($container) -- START" "INFO"
-docker run  \
-    --name=$container  \
-    --user='builder' \
-    -d -v /tmp/rpm:/mnt/rpm  \
-    -ti  $image  tail -f /dev/null &
+docker run -it --user='builder' --name=$container -d -v /tmp/rpm:/mnt/rpm $image tail -f /dev/null &
 
 if container_started; then
     std_message "Container ${container} started successfully" "OK"
