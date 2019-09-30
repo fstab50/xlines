bf9d1f25f1d896cc373c5ae9bfc193ac9ad52679
Blake Huber <blakeca00@gmail.com>
2019-09-30 08:11:35 -0500

    scripts/buildrpm-amzn2.sh: updates from centos7 buildrpm package script

--- scripts/buildrpm-amzn2.sh
+++ scripts/buildrpm-amzn2.sh
@@ -8,6 +8,53 @@
 #   Copyright 2019, Blake Huber
 #
 
+# --- globals --------------------------------------------------------------------------------------
+
+pkg=$(basename $0)                                  # pkg (script) full name
+pkg_root=$(echo $pkg | awk -F '.' '{print $1}')     # pkg without file extention
+pkg_path=$(cd $(dirname $0); pwd -P)                # location of pkg
+VERSION="$1"
+username="builder"
+home_dir="$(echo $HOME)"
+NOW=$(date +'%Y-%m-%d')
+BUILDDIR="$HOME/rpmbuild"
+VOLMNT="/mnt/rpm"
+LOG_DIR="$HOME/logs"
+LOG_FILE="$LOG_DIR/$pkg_root.log"
+QUIET="false"
+
+MAKE=$(which make)
+
+# colors
+brightwhite='\033[38;5;15m'
+cyan=$(tput setaf 6)
+green=$(tput setaf 2)
+red=$(tput setaf 1)
+yellow=$(tput setaf 3)
+BOLD='\u001b[1m'                          # ansi format
+bold='\u001b[1m'                          # ansi format
+underline='\u001b[4m'                     # ansi format
+
+# formatting
+title=$(echo -e ${bold}${brightwhite})    # title color, white + bold
+ul=$(echo -e ${underline})                # std underline
+bd=$(echo -e ${bold})                     # std bold
+wt=$(echo -e ${brightwhite})              # help menu accent 2
+fs=$(echo -e ${yellow})                   # file path color
+reset='\u001b[0m'                         # clear accents; return terminal colors
+
+# exit codes
+E_OK=0                                    # exit code if normal exit conditions
+E_DEPENDENCY=1                            # exit code if missing required ec2cdependency
+E_NOLOG=2                                 # exit code if failure to create log dir, log file
+E_BADSHELL=3                              # exit code if incorrect shell detected
+E_AUTH=4                                  # exit code if authentication failure
+E_USER_CANCEL=7                           # exit code if user cancel
+E_BADARG=8                                # exit code if bad input parameter
+E_NETWORK_ACCESS=9                        # exit code if no network access from current location
+E_EXPIRED_CREDS=10                        # exit code if temporary credentials no longer valid
+E_MISC=11                                 # exit code if miscellaneous (unspecified) error
+
 
 # --- declarations ------------------------------------------------------------
 
@@ -44,19 +91,59 @@ function export_package(){
 
     cd "$(_git_root)/dist"
 
-    package=$(find . -name "xlines-[0-9].[0-9].[0-9]-[0-9].noarch.rpm")
+    package=$(find . -name "python37-xlines-[0-9].[0-9].*-[0-9].noarch.rpm")
 
-    # prefix package name with python3 version
-    package=$(echo $package | cut -c 3-30)
-    p3_package="python36-$package"
+    # truncate additional ./ chars
+    package=$(echo $package | cut -c 3-50)
 
-    mv "$package" "$p3_package"
-    sudo cp "$p3_package" "$external/$p3_package"
+    cp "$package" "$external/$package"
+    return 0
 
-    if [[ -f "$external/$p3_package" ]]; then
+    if [[ -f "$external/$package" ]]; then
         return 0
     fi
     return 1
+}
+
+
+function increment_package_version(){
+    ##
+    ##  Runs version_update script to increment package
+    ##  version -- OR -- hard set package version id
+    ##
+    local _root="$1"
+    local version="$2"
+    local python3bin=$(which python3)
+
+    std_message "Initiating package version update (version $version)" $LOG_FILE
+    # $python3bin "$_root/scripts/version_update.py --hardset $version"
+    cd $_root && $python3bin "scripts/version_update.py"
+}
+
+
+function precheck(){
+    ##
+    ##  Validate and create prerun structures
+    ##
+    local dir="$1"
+
+    if [ ! -f "$dir" ]; then
+        mkdir -p $dir
+    fi
+
+    std_message "Installing Python3 libraries & dependencies" "INFO" $LOG_FILE
+    sudo -H $_PIP install -U libtools
+}
+
+function rpm_contents(){
+    ##
+    ##  Displays detailed view of all rpm contents
+    ##
+    local rpmfile="/home/builder/git/xlines/dist/python??-xlines*.noarch.rpm"
+    local contents="/home/builder/rpmbuild/RPMS/rpm-contents.txt"
+
+    rpm -qlpv $rpmfile > "$contents"
+    return 0
 }
 
 
@@ -69,57 +156,63 @@ _YUM=$(which yum)
 _SED=$(which sed)
 _PIP=$(_pip_exec)
 _POSTINSTALL=${ROOT}/packaging/rpm/rpm_postinstall.sh
-_REQUIRES='python3,python3-pip,python3-setuptools,bash-completion,which'
+REQUIRES='python3,python3-pip,python3-setuptools,bash-completion,which'
 
 # colors; functions
 . "$ROOT/scripts/colors.sh"
 . "$ROOT/scripts/std_functions.sh"
 
 
-if lsb_release -sirc | grep -i amazon >/dev/null 2>&1; then
+precheck "$LOG_DIR"
 
-    std_message "Dependency check: validate epel package repository installed" "INFO"
+increment_package_version "$ROOT" "$VERSION"
 
-    if [[ $(${_YUM} repolist 2>/dev/null | grep epel) ]]; then
-        std_message "epel Redhat extras packages repository installed." "OK"
+if lsb_release -sirc | grep -i centos >/dev/null 2>&1; then
+
+    # prerun update
+    cd $ROOT || exit 1
+    git pull
+
+    std_message "Dependency check: validate epel package repository installed" "INFO" $LOG_FILE
+
+    if [[ $(sudo ${_YUM} repolist 2>/dev/null | grep epel) ]]; then
+        std_message "epel Redhat extras packages repository installed." "OK" $LOG_FILE
     else
-        std_message "ERROR: epel Redhat extras packages repository NOT installed. Exit" "WARN"
+        std_message "ERROR: epel Redhat extras packages repository NOT installed. Exit" "WARN" $LOG_FILE
         exit 1
     fi
 
     # strip out sudo path restrictions
     sudo $_SED -i '/env_reset/d' /etc/sudoers
 
-    std_message "Installing packages" "INFO"
-    $_YUM -y install epel-release which sudo rpm-build
-    $_YUM -y install python3 python3-pip python3-setuptools python3-devel
+    std_message "Installing packages" "INFO" $LOG_FILE
+    sudo $_YUM -y install python3 python3-pip python3-setuptools which
 
-    std_message "Upgrade pip, setuptools" "INFO"
+    std_message "Upgrade pip, setuptools" "INFO" $LOG_FILE
     sudo -H $_PIP install -U pip setuptools
 
-    std_message "pip install pygments" "INFO"
-    sudo -H $_PIP install -U pygments
-
-    std_message "Coping setuptools lib from /usr/local/lib to /usr/lib/" "INFO"
+    std_message "Coping setuptools lib from /usr/local/lib to /usr/lib/" "INFO" $LOG_FILE
     sudo cp -r /usr/local/lib/python3.*/site-packages/setuptools* /usr/lib/python3.*/site-packages/
 
-    std_message "Coping pkg_resources lib from /usr/local/lib to /usr/lib/" "INFO"
+    std_message "Coping pkg_resources lib from /usr/local/lib to /usr/lib/" "INFO" $LOG_FILE
     sudo cp -r /usr/local/lib/python3.*/site-packages/pkg_resources* /usr/lib/python3.*/site-packages/
 
-    std_message "Coping pygments from /usr/local/lib64 to git_root" "INFO"
-    sudo cp -r /usr/local/lib64/python3.*/site-packages/Pygments*  .
-	sudo cp -r /usr/local/lib64/python3.*/site-packages/pygments*  .
-
     # python3 build process
-    $_PYTHON3_PATH setup_rpm.py bdist_rpm --requires=${_REQUIRES} \
+    $_PYTHON3_PATH setup_rpm.py bdist_rpm --requires=${REQUIRES} \
                                           --python='/usr/bin/python3' \
                                           --post-install=${_POSTINSTALL}
 
-    if export_package; then
-        exit 0
-    else
-        exit 1
-    fi
+    # process completed rpm
+    export_package
+
+    # output rpm contents
+    contents="RPMS/rpm-contents.txt"
+    std_message "RPM Contents:" "INFO" $LOG_FILE
+    rpm_contents
+
+    std_message "copy completed rpm to volume mount: $VOLMNT" "INFO" $LOG_FILE
+    cp -rv ~/rpmbuild/RPMS $VOLMNT/ >> $LOG_FILE
+
 else
     std_message "Not a Redhat-based Linux distribution. Exit" "WARN"
     exit 1
