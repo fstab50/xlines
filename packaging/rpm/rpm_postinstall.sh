8c61fbf6e994d28b192f8c70847d4980547e6eec
Blake Huber <blakeca00@gmail.com>
2019-10-05 13:08:36 -0500

    rpm_postinstall.sh: Rewrite of rpm pkg postinstall script to implement logging and more robust branching on error conditions

--- packaging/rpm/rpm_postinstall.sh
+++ packaging/rpm/rpm_postinstall.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 #
 #  xlines preinstall script :  Copyright 2018-2019, Blake Huber
 #
@@ -20,7 +19,19 @@
 #  contained in the program LICENSE file.
 #
 
+#
+#   Script Log Output
+#
+#       - Can be found in the system log location
+#       - typically /var/log/messages
+#
+#
+
 _project='xlines'
+loginfo='[INFO]'
+logwarn='[WARN]'
+
+declare -a os_packages
 
 
 function _pip_exec(){
@@ -61,39 +72,86 @@ function _amazonlinux(){
     ##
     ##  determines if Amazon Linux
     ##
-
     if [[ $(grep -i 'amazon linux' /etc/os-release) ]] && \
          [[ $(grep 'VERSION' /etc/os-release | grep '2') ]]; then
+        logger "$loginfo: Amazon Linux 2 OS type detected."
         return 0
     fi
     return 1
 }
 
 
+function python_dependencies(){
+    ##
+    ##  Validates if deps installed (true) or not (false)
+    ##
+    local pip_bin="$1"
+
+    if [[ $($pip_bin list | grep -i pygments) ]]; then
+        logger "$loginfo: Current pygments install detected, skipping fresh install"
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+function install_os_dependencies(){
+    ##
+    ##  Validates if deps installed (true) or not (false)
+    ##
+    declare -a packages=("${!1}")
+
+    for pkg in "${packages[@]}"; do
+        logger "$loginfo: Installing os package $pkg"
+        yum install -y $pkg
+    done
+}
+
+
 # --- main --------------------------------------------------------------------
 
 
+# install OS dependencies
+os_packages=(
+    "mlocate"
+)
+install_os_dependencies os_packages[@]
+
 # build and update locate db
+logger "$loginfo: Creating and updating local mlocate databases..."
 updatedb
 
 # locate pip executable
+logger "$loginfo: Locating Python3 pip executable..."
 _PIP=$(_pip_exec)
+if [[ $_PIP ]]; then
+    logger "$loginfo: pip executable path found: $_PIP"
+else
+    logger "$logwarn: failed to find pip executable path"
+fi
 
+logger "$loginfo: Set USER $USER ownership on .config directory..."
 if [ "$SUDO_USER" ]; then
     chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config
 else
     chown -R $USER:$USER /home/$USER/.config
 fi
 
-if _amazonlinux; then
+if _amazonlinux && ! python_dependencies $_PIP; then
+    logger "$loginfo: Amazon Linux 2 os detected, but missing Pygments library. Installing..."
     # install pygments
     $_PIP install pygments
 fi
 
 # generate bytecode artifacts
-if which py3compile >/dev/null; then
+if which py3compile >/dev/null 2>&1; then
+    logger "$loginfo: py3compile found... initating bytecode compilation"
     py3compile --package python3?-xlines
+else
+    logger "$loginfo: py3compile not found... skipping bytecode compilation"
 fi
 
+# <-- end python package postinstall script -->
 
 exit 0
