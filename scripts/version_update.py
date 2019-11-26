d9df66040d11631edc59aa4b824eff18f1e10a85
Blake Huber <blakeca00@gmail.com>
2019-11-25 18:10:55 -0600

    scripts/version_update.py: additional edge case error handling in python3 version module

--- scripts/version_update.py
+++ scripts/version_update.py
@@ -362,6 +362,12 @@ def main():
         help_menu()
         return True
 
+    elif args.dryrun and args.update:
+        stdout_message('Option --dryrun and --update cannot be used together.', prefix='FAIL')
+
+    elif args.set and not (args.update or args.dryrun):
+        stdout_message('--set-version must be used with --update or --dryrun.', prefix='FAIL')
+
     elif args.dryrun:
         # use version contained in pypi registry
         update_dryrun(PACKAGE, module, args.set, args.debug)
