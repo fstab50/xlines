87aa753e576508a2b431375c9e840f92834ca76e
Blake Huber <blakeca00@gmail.com>
2019-12-03 18:05:36 -0600

    packaging/rpm/rpm_postinstall.sh: add path to pip3 exec search

--- packaging/rpm/rpm_postinstall.sh
+++ packaging/rpm/rpm_postinstall.sh
@@ -58,6 +58,12 @@ function _pip_exec(){
         logger "$loginfo: pip3 executable path found: $_pip"
         return 0
 
+    elif [[ $(/bin/pip3 --version 2>/dev/null | grep "python\ 3.[6-9]") ]]; then
+        _pip="/bin/pip3"
+        echo "$_pip"
+        logger "$loginfo: pip3 executable path found: $_pip"
+        return 0
+
     elif [[ $(/usr/local/bin/pip --version 2>/dev/null | grep "python\ 3.[6-9]") ]]; then
         _pip="/usr/local/bin/pip"
         echo "$_pip"
