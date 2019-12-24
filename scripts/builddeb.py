3f317a2715863e17f93ca08ea16d37ce2172a153
Blake Huber <blakeca00@gmail.com>
2019-12-24 09:08:06 -0600

    try copy of modules from repo location instead of venv

--- scripts/builddeb.py
+++ scripts/builddeb.py
@@ -469,15 +469,16 @@ def builddir_structure(param_dict, builddir, version):
         if not os.path.exists(lib_dst):
             os.makedirs(lib_dst)
 
-        for lib in os.listdir(lib_src):
-            _src = os.path.join(lib_src, lib)
-            _dst = os.path.join(lib_dst, PROJECT_BIN, lib)
-            copyfile(_src, _dst)
+        #for lib in os.listdir(lib_src):
+        #    _src = os.path.join(lib_src, lib)
+        #    _dst = os.path.join(lib_dst, PROJECT_BIN, lib)
+        #    copyfile(_src, _dst)
+        copytree(lib_src, lib_dst + '/' + PROJECT_BIN)
 
-            stdout_message(
-                    message='Copied:\t{} {} {}'.format(lk + _src + rst, arrow, lk + lib_dst + rst),
-                    prefix='OK'
-                )
+        stdout_message(
+                message='Copied:\t{} {} {}'.format(lk + _src + rst, arrow, lk + lib_dst + rst),
+                prefix='OK'
+            )
 
         if not os.path.exists(comp_dst):
             # create path
